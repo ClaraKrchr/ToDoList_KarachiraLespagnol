a0e132a8d90410d79ef21cbfdce31b8bde008139
Vylkhin <Vylkhin@live.fr>
2020-12-01 12:06:19 +0100

    Affichage des tasks

--- app/src/main/java/com/karachira/todolist_karachiralespagnol/SecondActivity.java
+++ app/src/main/java/com/karachira/todolist_karachiralespagnol/SecondActivity.java
@@ -2,7 +2,9 @@ package com.karachira.todolist_karachiralespagnol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SecondActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
