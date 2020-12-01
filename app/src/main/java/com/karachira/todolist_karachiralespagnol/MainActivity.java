0c835d1577b2cdac2254d32ac69aa083b7b5ec58
ClaraKrchr <clara.krchr@gmail.com>
2020-12-01 09:34:27 +0100

    Initial commit

---
+++ app/src/main/java/com/karachira/todolist_karachiralespagnol/MainActivity.java
@@ -0,0 +1,14 @@
+package com.karachira.todolist_karachiralespagnol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
